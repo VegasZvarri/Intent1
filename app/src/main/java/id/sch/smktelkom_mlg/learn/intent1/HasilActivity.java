9e6191424f257b6032b69b794615615280bad776
Sherlock Holmes <vegaszvarri@gmail.com>
2016-12-07 19:35:53 +0700

    StartActivity Part 2.

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -3,25 +3,46 @@ package id.sch.smktelkom_mlg.learn.intent1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class HasilActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hasil2);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_hasil2);
 
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==android.R.id.home){
-            OnBackPressed();
-            return true;
+            findViewById(R.id.buttonBack).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onBackPressed();
+                }
+            });
+
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+            String nama = getIntent().getStringExtra(MainActivity.NAMA);
+            int umur = getIntent().getIntExtra(MainActivity.UMUR,0);
+
+            int yearNow = Calendar.getInstance().get(Calendar.YEAR);
+            int tahunLahir = yearNow - umur;
+
+            TextView tvHasil = (TextView) findViewById(R.id.textViewHasil);
+            tvHasil.setText(nama + " lahir pada tahun " + tahunLahir);
+
+            setTitle("Hasil");
         }
-        return super.onOptionsItemSelected(item);
-    }
 
-    private void OnBackPressed() {
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item)
+        {
+            if (item.getItemId()==android.R.id.home){
+                onBackPressed();
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
     }
-}
